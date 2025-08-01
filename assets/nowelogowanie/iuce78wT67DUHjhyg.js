fd4106f307ad726a62331c64ae1f20527b094dc7
bed-deb <julus06@o2.pl>
2025-08-01 19:53:02 +0200

    Update iuce78wT67DUHjhyg.js

--- assets/nowelogowanie/iuce78wT67DUHjhyg.js
+++ assets/nowelogowanie/iuce78wT67DUHjhyg.js
@@ -2,7 +2,7 @@ function login() {
         var password = document.getElementById('password').value;
 
         // Tablica przechowująca poprawne hasła
-        var correctPasswords = ["Xv#83!Lp$eW6@zRt", "polak"]; // Możesz dodać więcej haseł tutaj
+        var correctPasswords = ["Xv#83!Lp$eW6@zRt", "Zx_7!k$Tr@v#LpQ", "polak"]; // Możesz dodać więcej haseł tutaj
 
         // Sprawdzenie, czy wprowadzone hasło znajduje się w tablicy
         if (password === "") {
